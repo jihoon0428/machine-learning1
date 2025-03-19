f61d06ee80c23c2561b8b5b510cd3212156d9b42
최지훈 <ji040304@naver.com>
2025-03-19 13:01:21 +0900

    Add files via upload
    cp.01

---
+++ 수업코드/MR 1.R
@@ -0,0 +1,19 @@
+midtest <- c(75, 88, 91, 68, 82)
+lasttest<- c(60, 87, 55, 47, 92)
+mean(midtest)
+mean(lasttest)
+
+midtest_1 <- c(75, 88, 82)
+lasttest_1<- c(60, 87, 55)
+mean(midtest_1)
+mean(lasttest_1)
+
+midtest <- c(75, 88, 91, 68, 82)
+lasttest<- c(60, 87, 55, 47, 92)
+midtest*0.5
+lasttest*0.3
+
+year <- c(5, 5.5, 7, 10)
+(((5.5/5)*(7/5.5)*(10/7))^1/3)-1
+median(year)
+
